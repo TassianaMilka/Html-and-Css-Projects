0a45d5f2ab0c519511cfa2d2beed04f2a839e394
Tassiana Milka <114196099+TassianaMilka@users.noreply.github.com>
2024-07-19 00:26:35 -0300

    1

---
+++ Saber Número/js/depoisdainicial.js
@@ -0,0 +1,14 @@
+/*Destina para aparecer um lembrente*/
+alert("Aqui vai aparecer o resultado\n");  
+/*Aconteceu de declarar uma string direto com o nome dela*/
+var nome = "Saber o número"; 
+var numero=26; 
+document.write("\n\t\tO nome da String\n="+nome)
+/*Validação de dado se o número é maior igual ou menor igual*/
+document.write("\n");
+if(numero<= 12) 
+ document.write("\n\tO número é menor\n="+numero); 
+
+else if(numero>=26){
+ document.write("\n\t\tO número é maior\n=" + nome+numero); 
+} 
